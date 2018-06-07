16f8742bf8261c489616ba82a869750ead96d489
nkazakova <natalia.kazakova@innoway.ru>
2018-06-07 19:00:04 +0300

    added JSON parsing data

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,41 +1,28 @@
 package com.example.android.quakereport;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 /**
  * Created by Kazakova_net on 06.06.2018.
  */
 public class Earthquake {
     private double magnitude;
     private String place;
-    private Date date;
-
-    Earthquake(double magnitude, String place, String date) {
+    private String time;
+    
+    Earthquake(double magnitude, String place, String time) {
         this.magnitude = magnitude;
         this.place = place;
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        try {
-            this.date = simpleDateFormat.parse(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        this.time = time;
     }
-
+    
     public double getMagnitude() {
         return magnitude;
     }
-
+    
     public String getPlace() {
         return place;
     }
-
+    
     public String getDate() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM d, YYYY", Locale.getDefault());
-
-        return simpleDateFormat.format(date);
+        return time;
     }
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,37 +17,28 @@ package com.example.android.quakereport;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class EarthquakeActivity extends AppCompatActivity {
-
+    
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
-
+        
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakeArrayList = new ArrayList<>();
-        earthquakeArrayList.add(new Earthquake(7.2, "San Francisco", "2016-02-02"));
-        earthquakeArrayList.add(new Earthquake(6.1, "London", "2015-07-20"));
-        earthquakeArrayList.add(new Earthquake(3.9, "Tokyo", "2014-11-10"));
-        earthquakeArrayList.add(new Earthquake(5.4, "Mexico City", "2014-05-03"));
-        earthquakeArrayList.add(new Earthquake(2.8, "Moscow", "2013-01-31"));
-        earthquakeArrayList.add(new Earthquake(4.9, "Rio de Janeiro", "2012-08-19"));
-        earthquakeArrayList.add(new Earthquake(1.6, "Paris", "2011-10-30"));
-
+        ArrayList<Earthquake> earthquakeArrayList = QueryUtils.extractEarthquakes();
+        
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
+        
         // Create a new {@link ArrayAdapter} of earthquakeArrayList
         EarthquakeArrayAdapter earthquakeArrayAdapter = new EarthquakeArrayAdapter(EarthquakeActivity.this, earthquakeArrayList);
-
+        
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(earthquakeArrayAdapter);
