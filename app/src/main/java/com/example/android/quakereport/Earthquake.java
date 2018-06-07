6395151afd3061008f10d7577c45a169af863f0e
kazakovaNet <kazakova.net@yandex.ru>
2018-06-08 00:40:08 +0300

    Split location into 2 TextViews

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -7,7 +7,15 @@ public class Earthquake {
     private double magnitude;
     private String place;
     private long time;
-    
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param place is the city location of the earthquake
+     * @param time is the time in milliseconds (from the Epoch) when the
+     *  earthquake happened
+     */
     Earthquake(double magnitude, String place, long time) {
         this.magnitude = magnitude;
         this.place = place;
--- app/src/main/java/com/example/android/quakereport/EarthquakeArrayAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeArrayAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -13,13 +12,14 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 /**
  * Created by Kazakova_net on 06.06.2018.
  */
 public class EarthquakeArrayAdapter extends ArrayAdapter<Earthquake> {
+    private static final String LOCATION_SEPARATOR = " of ";
+
     public EarthquakeArrayAdapter(@NonNull Activity context, @NonNull ArrayList<Earthquake> earthquakes) {
         super(context, 0, earthquakes);
     }
@@ -33,20 +33,64 @@ public class EarthquakeArrayAdapter extends ArrayAdapter<Earthquake> {
         }
 
         TextView magnitudeTextView = listItemView.findViewById(R.id.magnitude_text_view);
-        TextView placeTextView = listItemView.findViewById(R.id.place_text_view);
+        TextView locationOffsetTextView = listItemView.findViewById(R.id.location_offset_text_view);
+        TextView primaryLocationTextView = listItemView.findViewById(R.id.primary_location_text_view);
         TextView dateTextView = listItemView.findViewById(R.id.date_text_view);
+        TextView timeTextView = listItemView.findViewById(R.id.time_text_view);
 
         Earthquake earthquake = getItem(position);
         if (earthquake != null) {
             magnitudeTextView.setText(String.valueOf(earthquake.getMagnitude()));
-            placeTextView.setText(earthquake.getPlace());
-            
+
+            locationOffsetTextView.setText(formatLocationOffset(earthquake.getPlace()));
+            primaryLocationTextView.setText(formatPrimaryLocation(earthquake.getPlace()));
+
             Date earthquakeDate = new Date(earthquake.getDate());
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd, YYYY", Locale.getDefault());
-            
-            dateTextView.setText(simpleDateFormat.format(earthquakeDate));
+
+            dateTextView.setText(formatDate(earthquakeDate));
+            timeTextView.setText(formatTime(earthquakeDate));
         }
 
         return listItemView;
+    }
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy", Locale.getDefault());
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a", Locale.getDefault());
+        return timeFormat.format(dateObject);
+    }
+
+    private String formatLocationOffset(String place) {
+        if (place.length() == 0) {
+            return "";
+        }
+
+        if (place.contains(LOCATION_SEPARATOR)) {
+            return place.substring(0, place.indexOf(LOCATION_SEPARATOR) + LOCATION_SEPARATOR.length());
+        }
+
+        return getContext().getString(R.string.near_the);
+    }
+
+    private String formatPrimaryLocation(String place) {
+        if (place.length() == 0) {
+            return "";
+        }
+
+        if (place.contains(LOCATION_SEPARATOR)) {
+            return place.substring(place.indexOf(LOCATION_SEPARATOR) + LOCATION_SEPARATOR.length(), place.length());
+        }
+
+        return place;
     }
 }
